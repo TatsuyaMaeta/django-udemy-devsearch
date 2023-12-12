d6c73211895d1f2563e5967a788d654d8a13d274
TatsuyaMaeta <tat.mae084@gmail.com>
2023-12-13 00:18:51 +0900

    add query method

--- app/app/README.md
+++ app/app/README.md
@@ -5,3 +5,12 @@ project外のtemplateフォルダの設定の仕方
 
 テンプレートのdocs
 https://docs.djangoproject.com/ja/4.2/topics/templates/
+
+
+
+queryset object method
+link[link](https://docs.djangoproject.com/en/5.0/ref/models/querysets/#queryset-api)
+1. ModelName.objects.all()
+2. ModelName.objects.get()
+3. ModelName.objects.filter()
+4. ModelName.objects.exclude()
--- app/app/projects/views.py
+++ app/app/projects/views.py
@@ -1,24 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
 
-projectList = [
-    {"id": 1, "title": "hoge", "description": "hogehoge"},
-    {"id": 2, "title": "fuga", "description": "fugafuga"},
-    {"id": 3, "title": "bar", "description": "barbar"},
-]
+# projectList = [
+#     {"id": 1, "title": "hoge", "description": "hogehoge"},
+#     {"id": 2, "title": "fuga", "description": "fugafuga"},
+#     {"id": 3, "title": "bar", "description": "barbar"},
+# ]
 
 
 def projects(request):
-    msg = "Projects"
-    number = 100
-    context = {"msg": msg, "num": number, "projects": projectList}
+    # msg = "Projects"
+    # number = 100
+    # context = {"msg": msg, "num": number, "projects": projectList}
+    project = Project.objects.all()
+    context = {"projects": project}
     return render(request, "projects/projects.html", context)
 
 
 def project(request, pk):
-    projectObj = None
-    for i in projectList:
-        if i["id"] == int(pk):
-            projectObj = i
+    # projectObj = None
+    # for i in projectList:
+    #     if i["id"] == int(pk):
+    #         projectObj = i
+    projectObj = Project.objects.get(id=pk)
+    # 上記のprojectObjに含まれているtagsのカラムの要素を全て取得している
+    tags = projectObj.tags.all()
 
-    return render(request, "projects/single-project.html", {"project": projectObj})
+    return render(
+        request, "projects/single-project.html", {"project": projectObj, "tags": tags}
+    )
